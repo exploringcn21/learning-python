d289ec2ff98c76817b9861729d584d5751816f5d
exploringcn21 <exploringcn21@gmail.com>
2021-07-25 20:20:56 +0100

    optimize user input

--- 01-simple-english-thesaurus/app.py
+++ 01-simple-english-thesaurus/app.py
@@ -10,7 +10,13 @@ def get_meaning(w):
     else:
         possible_words = get_close_matches(word=w, possibilities=data.keys(), cutoff=0.75)
         if len(possible_words) > 0:
-            return f"No such word exists. Did you mean '{possible_words[0]}'?"
+            user_response = input(f"No such word exists. Did you mean '{possible_words[0]}'? Enter yes/no: ")
+            if user_response.lower() == "yes":
+                return data[possible_words[0]]
+            elif user_response.lower() == "no":
+                return "No such word exists. Please enter a valid word."
+            else:
+                return "Sorry. Please enter 'yes' or 'no' as a response."
         else:
             return "No such word exists. Please enter a valid word."
 
