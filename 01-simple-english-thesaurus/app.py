754daae153492f16a72887e61c1da82df8e90fe7
exploringcn21 <exploringcn21@gmail.com>
2021-07-25 19:51:55 +0100

    suggest similar words for incorrect input

--- 01-simple-english-thesaurus/app.py
+++ 01-simple-english-thesaurus/app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("resources/data.json"))
 
@@ -7,7 +8,12 @@ def get_meaning(w):
     if w in data:
         return data[w]
     else:
-        return "No such word exists. Please enter a valid word."
+        possible_words = get_close_matches(word=w, possibilities=data.keys(), cutoff=0.75)
+        if len(possible_words) > 0:
+            return f"No such word exists. Did you mean '{possible_words[0]}'?"
+        else:
+            return "No such word exists. Please enter a valid word."
+
 
 word = input("Enter word: ")
 print(get_meaning(word))
