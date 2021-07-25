6489e8c709f88273193d6fa99803dd9cd20539b1
exploringcn21 <exploringcn21@gmail.com>
2021-07-25 19:19:03 +0100

    check validity of entered word

--- 01-simple-english-thesaurus/app.py
+++ 01-simple-english-thesaurus/app.py
@@ -1,9 +1,12 @@
 import json
 
-data = json.load(open('resources/data.json'))
+data = json.load(open("resources/data.json"))
 
 def get_meaning(w):    
-    return data[w]
+    if w in data:
+        return data[w]
+    else:
+        return "No such word exists. Please enter a valid word."
 
-word = input('Enter word: ')
+word = input("Enter word: ")
 print(get_meaning(word))
